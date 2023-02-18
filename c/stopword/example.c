cebce12d7ed0313d4aa8fb82d4757d4ee81e2e02
Darius Runge <dariusrunge@magenta.de>
2023-02-18 19:46:16 +0100

    Improved example try-out program

--- c/stopword/example.c
+++ c/stopword/example.c
@@ -4,23 +4,22 @@
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
 
+    wchar_t buffer[STOPWORD_BUFFER_SIZE];
+
+    if(argc != 2) {
+        wprintf(L"Usage: %s [word]\n", argv[0]);
+        return 0;
+    }
+
+    swprintf(buffer, STOPWORD_BUFFER_SIZE, L"%s", argv[1]);
+
     if(load_stopwords("german.csv") == -1) {
         wprintf(L"Unable to load file.\n");
     } else {
-        if(is_stopword(L"aller")) {
-            wprintf(L"'aller' is a stopword\n");
-        }
-
-        if(is_stopword(L"Aller")) {
-            wprintf(L"'Aller' is a stopword\n");
-        }
-
-        if(is_stopword(L"derzeitiger")) {
-            wprintf(L"'derzeitiger' is a stopword\n");
-        }
-
-        if(is_stopword(L"Derzeitiger")) {
-            wprintf(L"'Derzeitiger' is a stopword\n");
+        if(is_stopword(buffer)) {
+            wprintf(L"'%ls' is a stopword\n", buffer);
+        } else {
+            wprintf(L"'%ls' is not a stopword\n", buffer);
         }
 
         free_stopwords();
