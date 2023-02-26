eae88f15881ffab1c4ccae11f676545841329929
Darius Runge <darius-runge@magenta.de>
2023-02-26 13:25:20 +0100

    Made generic cmd-arg-based test program and fixed ref to pir_t in line 21

--- c/pir-code/example.c
+++ c/pir-code/example.c
@@ -5,13 +5,20 @@
 
 int main(int argc, char* argv[]) {
 	pir_t out;
+	wchar_t comp[255];
 
 	setlocale(LC_ALL, "");
 
-	pir_code_init(&out);
-	pir_code_write(L"Lyssavirus", &out);
+	if(argc != 2) {
+		return -1;
+	}
 
-	wprintf(L"%.*s\n", PIR_CODE_LENGTH, &out);
+	swprintf(comp, 255, L"%s", argv[1]);
+
+	pir_code_init(&out);
+	pir_code_write(comp, &out);
+
+	wprintf(L"%.*s\n", PIR_CODE_LENGTH, out.pir);
 	
 	return 0;
 }
