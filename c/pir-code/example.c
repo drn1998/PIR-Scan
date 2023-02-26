df698e61fe34fddcff3ed18c96b771b4fb31480d
Darius Runge <darius-runge@magenta.de>
2023-02-26 13:11:34 +0100

    Updated the PIR code module

--- c/pir-code/example.c
+++ c/pir-code/example.c
@@ -4,12 +4,14 @@
 #include <locale.h>
 
 int main(int argc, char* argv[]) {
-	char out[PIR_CODE_LENGTH];
+	pir_t out;
 
 	setlocale(LC_ALL, "");
 
-	write_pir_code(L"lyssavirus", out);
-	wprintf(L"%.*s\n", PIR_CODE_LENGTH, out);
+	pir_code_init(&out);
+	pir_code_write(L"Lyssavirus", &out);
+
+	wprintf(L"%.*s\n", PIR_CODE_LENGTH, &out);
 	
 	return 0;
 }
