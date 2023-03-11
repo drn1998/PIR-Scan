34a8555876e058d5857353cbe51b1d9275c48a84
Darius Runge <darius-runge@magenta.de>
2023-03-11 18:14:31 +0100

    Added test for sequence only made of stopwords

--- c/eval/test/run.sh
+++ c/eval/test/run.sh
@@ -1,6 +1,6 @@
 # Compile and run tests with valgrind
 
-for i in {1..9}
+for i in {1..10}
 do
     gcc test_$(printf "%02d" $i).c ../eval.c ../../pir-code/pir.c ../../stopword/stopword.c ../../stem/stem.c ../../string-util/string-util.c -Wall -Wextra -fanalyzer -O3 -g -o test_$i
     valgrind -q ./test_$i
